a4f22029f40bb19c6467814b1ab91458cf88d6a2
AaronUCR <aaron.arcealfaro@ucr.ac.cr>
2024-04-25 00:49:10 -0600

    Creacion funcion precedencia y comentarios de tareas a realizar

--- src/shunting_yard.cc
+++ src/shunting_yard.cc
@@ -15,10 +15,40 @@ shunting_yard::shunting_yard(tokenizer tokenList): tokenList(tokenList){
         if(token.isNumber()){
             output_Queue.push(token);
             cout<<"Leyendo:"<<token.getNumber()<<endl;
-            //tokenQueue.pop();
+            tokenQueue.pop();
         }
         else if(token.isOperator()){
+        /*Mientras que el operador a ingresar sea de mayor precedencia, 
+            pushee los operadores que estaban en el stack a la cola de salida
+        */
 
+       // al final pushee ese operador actual al stack
+
+       /*Si es un bracket izquierdo, anadalo al stack
+       */
+
+      //Si es un bracket derecho
+
+      /*
+      Mientras que no haya un bracket izquierdo en el stack
+        Ponga elementos del stack a la cola de salida
+      */
+
+     //Quite el bracket izquierdo del stack
+     
+     //Mientras hayan operadores en el stack, anadalos a la pila
         }
     }
+}
+
+//Terminar y ver como se hara la precedencia
+int shunting_yard::getPrecedence(Token tokenOperator){
+    string tokenOp = tokenOperator.getValue();
+    if(tokenOp == "+"){
+        return 1;
+    }
+}
+
+queue <Token>shunting_yard::getOutputQueue(){
+    return output_Queue;
 }
