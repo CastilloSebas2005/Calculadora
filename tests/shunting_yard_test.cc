0a59560348a8dcd7a8cbce96110709827f74eb1e
Sebastián <sebastian.orozcocastillo@ucr.ac.cr>
2024-04-29 21:09:23 -0600

    Primer prueba unitaria de shunting yard

--- tests/shunting_yard_test.cc
+++ tests/shunting_yard_test.cc
@@ -4,4 +4,10 @@
 #include <shunting_yard.hh>
 #include <token.hh>
 
-
+TEST(shunting_yardTest1, sum){ 
+    tokenizer test("()");
+    queue <Token> output = test.getList();
+    shunting_yard testR (output);
+    queue <Token> verificator = testR.getOutputQueue();
+    EXPECT_FALSE(!verificator.empty());
+}
