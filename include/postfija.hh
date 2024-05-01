5bacfdfb18ca7ef6ad6ca8073fe1f752159446c1
joseaguerrar <jose.guerrarodriguez@ucr.ac.cr>
2024-04-30 22:20:03 -0600

    Implementación de la postfija con Clang, todo: pruebas unitarias

--- include/postfija.hh
+++ include/postfija.hh
@@ -1,21 +1,21 @@
 #ifndef POSTFIJA_HH
 #define POSTFIJA_HH
 
-#include <stack>
 #include <queue>
-#include<token.hh>
+#include <stack>
 #include <string>
+#include <token.hh>
 
-class postfija{
-    public:
-        postfija(queue <Token> tokenqueue);
-        double getResult();
-    private:
-        double result;
-        queue <Token> tokenQueue;
+class postfija {
+public:
+  postfija(queue<Token> tokenqueue);
+  double getResult();
+  string showError();
 
+private:
+  string error;
+  double result;
+  queue<Token> tokenQueue;
 };
-
-
 
 #endif
--- src/postfija.cc
+++ src/postfija.cc
@@ -1,61 +1,99 @@
-#include <stack>
-#include <queue>
-#include <token.hh>
-#include <string>
-#include <postfija.hh>
 #include <cmath>
 #include <iostream>
+#include <postfija.hh>
+#include <queue>
+#include <stack>
+#include <string>
+#include <token.hh>
 
-postfija::postfija(queue <Token> tokenqueue):tokenQueue(tokenqueue){
-    stack <Token> tokenStack;
-    double Result = 0;
-    while(!tokenqueue.empty()){
-        Token token = tokenqueue.front();
-        tokenqueue.pop();
-        if(token.isNumber()){
-            tokenStack.push(token);
+postfija::postfija(queue<Token> tokenqueue) : tokenQueue(tokenqueue) {
+  stack<Token> tokenStack;
+  double Result = 0;
+  string error = " ";
+  while (!tokenqueue.empty()) {
+    // Mientras no esté vacía
+    Token token = tokenqueue.front();
+    tokenqueue.pop();
+    if (token.isNumber()) {
+      // Si es número lo incluyo de una vez
+      tokenStack.push(token);
+    } else if (token.isOperator()) {
+      // Si es operador, encuentro el primer operando y luego el segundo
+      Token operand2 = tokenStack.top();
+      tokenStack.pop();
+      Token operand1 = tokenStack.top();
+      tokenStack.pop();
+      string operation = token.getValue();
+      // se usa operation[0] para poder hacer el switch y comparar como
+      // caractéres
+      switch (operation[0]) {
+      case '+':
+        // suma
+        Result = operand1.getNumber() + operand2.getNumber();
+        break;
+      case '-':
+        // resta
+        Result = operand1.getNumber() - operand2.getNumber();
+        break;
+      case '*':
+        // Multiplicación
+        Result = operand1.getNumber() * operand2.getNumber();
+        break;
+      case '/':
+        // División
+        if (operand2.getNumber() == 0)
+        // excepción de división entre 0
+        {
+          error = "Dividir entre 0 es indefinido...";
+          showError();
+          break;
+        } else {
+          Result = operand1.getNumber() / operand2.getNumber();
+          break;
         }
-        else if(token.isOperator()){
-            Token operand2 = tokenStack.top();
-            tokenStack.pop();
-            Token operand1 = tokenStack.top();
-            tokenStack.pop();
-            string operation = token.getValue();
-            //se usa operation[0] para poder hacer el switch y comparar como caractéres
-            switch (operation[0])
-            {
-            case '+':
-                Result = operand1.getNumber() + operand2.getNumber();
-                break;
-            case '-':
-                Result = operand1.getNumber() - operand2.getNumber();
-                break;
-            case '*':
-                Result = operand1.getNumber() * operand2.getNumber();
-                break;
-            case '/':
-                Result = operand1.getNumber() / operand2.getNumber();
-                break;
-            case '_':
-                Result = log(operand1.getNumber())/log(operand2.getNumber());
-                break;
-            case '^':
-                Result = pow(operand1.getNumber(), operand2.getNumber());
-                break;
-            case 'v':
-                Result = pow(operand2.getNumber(), (1/operand1.getNumber()));
-            default:
-                break;
-            }
-            Token saveResult(TokenType::TOKEN_TYPE_NUMBER, to_string(Result));
-            tokenStack.push(saveResult);
+      case '_':
+        // Logaritmo
+        if (operand1.getNumber() == 0 || operand2.getNumber() == 0)
+        // excepción con log de 0
+        {
+          error = "Logaritmo de 0 es indefinido...";
+          showError();
+          break;
+        } else {
+          Result = log(operand1.getNumber()) / log(operand2.getNumber());
+          break;
         }
-        if(tokenStack.size() == 1){
-            Token Result = tokenStack.top();
-            result = Result.getNumber();
+
+      case '^':
+        // Potencia
+        Result = pow(operand1.getNumber(), operand2.getNumber());
+        break;
+      case 'v':
+        // Raíz
+        if (operand2.getNumber() < 0 && int(operand1.getNumber()) % 2 == 0)
+        // excepción con numeros negativos
+        {
+          error = "No trabajamos con numeros imaginarios...";
+          showError();
+          break;
+        } else {
+          Result = pow(operand2.getNumber(), (1 / operand1.getNumber()));
+          break;
         }
+
+      default:
+        error = "No introdujo operadores válidos...";
+        showError();
+        break;
+      }
+      Token saveResult(TokenType::TOKEN_TYPE_NUMBER, to_string(Result));
+      tokenStack.push(saveResult);
     }
-
+    if (tokenStack.size() == 1) {
+      Token Result = tokenStack.top();
+      result = Result.getNumber();
+    }
+  }
 }
-
-double postfija::getResult(){return result;}
+string postfija::showError() { return error; }
+double postfija::getResult() { return result; }
